1c1b99aa1e51b2eaa81ab1f8c022504614b8488e
akameshev <cryptomonster161193@gmail.com>
2024-02-11 20:05:19 +0600

    added files

---
+++ Lesson1/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Console.WriteLine("Hi My name is Anuar");
+Console.WriteLine("Некоторый текст");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char q = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(q);
+Console.WriteLine(b);
---
+++ Lesson1/Task5/Program.cs
@@ -0,0 +1,9 @@
+﻿int N = 100;
+int i = 1;
+
+while (i <= N)
+{
+    //Console.WriteLine(i);
+    Console.Write(i + " ");
+    i = i + 1;
+}
