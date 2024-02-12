200d87d6b33a57e2afff2567da7e236d73c21166
akameshev <cryptomonster161193@gmail.com>
2024-02-12 22:34:17 +0600

    modified Tasks:Seminar2:Task2

--- Tasks/Seminar2/Task2/Program.cs
+++ Tasks/Seminar2/Task2/Program.cs
@@ -2,7 +2,11 @@
 int numberX = Convert.ToInt32(System.Console.ReadLine());
 System.Console.WriteLine("Введите значение для оси Y: ");
 int numberY = Convert.ToInt32(System.Console.ReadLine());
-if (numberX>0 && numberY>0)System.Console.WriteLine("точка координат находится в 1 четверти");
-else if(numberX<0 && numberY>0)System.Console.WriteLine("точка координат находится в 2 четверти");
-else if(numberX<0 && numberY<0)System.Console.WriteLine("точка координат находится в 3 четверти");
-else System.Console.WriteLine("точка координат находится в 4 четверти");
+if (numberX != 0 && numberY != 0)
+{
+    if (numberX > 0 && numberY > 0) System.Console.WriteLine("точка координат находится в 1 четверти");
+    else if (numberX < 0 && numberY > 0) System.Console.WriteLine("точка координат находится в 2 четверти");
+    else if (numberX < 0 && numberY < 0) System.Console.WriteLine("точка координат находится в 3 четверти");
+    else System.Console.WriteLine("точка координат находится в 4 четверти");
+}
+else System.Console.WriteLine("X и Y не могут быть равны нулю");
